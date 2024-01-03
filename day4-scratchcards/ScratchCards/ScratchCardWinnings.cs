80c7038b61ba4c1bca9758d61b63d3db60758b2d
Hannah Tsoi <hannah.tsoi+work@protonmail.com>
2024-01-03 22:28:12 +0000

    Day 4 - Initial setup and test

---
+++ day4-scratchcards/ScratchCards/ScratchCardWinnings.cs
@@ -0,0 +1,10 @@
+namespace ScratchCards;
+
+public static class ScratchCardsWinnings
+{
+
+    public static int CalculateWinnings(IEnumerable<string> scratchCardLines)
+    {
+        return 0;
+    }
+}
